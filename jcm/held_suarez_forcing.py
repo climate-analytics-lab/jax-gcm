6b0accae0be6dc6f9d1bc0ac9d3a42937c059c7b
eldavenport <ellendavenport7@gmail.com>
2024-08-15 19:36:19 -0700

    adding a shell of something that could work

--- jcm/held_suarez_forcing.py
+++ jcm/held_suarez_forcing.py
@@ -1,7 +1,25 @@
 from jcm.physics import PhysicsTendency, PhysicsState
+'''
+we need to return an instance of the Physics Tendency:
+
+class PhysicsTendency:
+    u_wind: jnp.ndarray
+    v_wind: jnp.ndarray
+    temperature: jnp.ndarray
+    specific_humidity: jnp.ndarray
+
+Held-Saurez 1994
+
+'''
 
 def held_suarez_forcings(state: PhysicsState):
-    d_temperature = state.temperature * sdfidsi
-    d_v = kdfknds
+    hsf = HeldSuarezForcing()
 
-    return PhysicsTendency(d_temperature, )
+    Teq = hsf.equilibrium_temperature(state.surface_pressure)
+    d_temperature = -hsf.kt() * (state.surface_temperature - Teq)
+
+    d_v_wind = -hsf.kv() * state.v_wind
+    d_u_wind = -hsf.kv() * state.v_wind
+    d_spec_humidity = 0 # just keep the same specific humidity?
+
+    return PhysicsTendency(d_u_wind, d_v_wind, d_temperature, d_spec_humidity)
