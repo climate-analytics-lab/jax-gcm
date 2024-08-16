91af277de455ced64af057d6f8107ba8284c4390
Veeramakali Vignesh Manivannan <45564838+veeramakalivignesh@users.noreply.github.com>
2024-08-15 21:21:40 -0700

    Get zonal avg fields (#25)
    * first version of get zonal average fields
    
    * get_avg_zonal_fields: second version, without tests
    
    * 2d array fix
    
    * using vmaps
    
    * sample tests
    
    * test cases basic: get_zonal_avg_fields
    
    * vmap changes
    
    * pulled in solar changes
    
    * removed comments
    
    * added solc
    
    ---------
    
    Co-authored-by: Rohith Vutukuru <rohithvutukuru267@gmail.com>
    Co-authored-by: Duncan Watson-Parris <dwatsonparris@ucsd.edu>

--- jcm/shortwave_radiation.py
+++ jcm/shortwave_radiation.py
@@ -1,5 +1,11 @@
 import jax.numpy as jnp
+from jax import jit
+from jax import vmap
+from jcm.physical_constants import epssw
+from jcm.params import il, ix
+from jcm.geometry import sia, coa
 import tree_math
+
 @tree_math.struct
 class SWRadiationData:
     qcloud: jnp.ndarray
@@ -8,28 +14,101 @@ class SWRadiationData:
     ozupp: jnp.ndarray
     zenit: jnp.ndarray
     stratz: jnp.ndarray
+
+
+@jit
+def get_zonal_average_fields(tyear):
+    """
+    Calculate zonal average fields including solar radiation, ozone depth, 
+    and polar night cooling in the stratosphere using JAX.
     
+    Parameters:
+    tyear : float
+        Time as fraction of year (0-1, 0 = 1 Jan)
+
+    Returns:
+    fsol : jnp.ndarray
+        Solar radiation at the top
+    ozupp : jnp.ndarray
+        Ozone depth in upper stratosphere
+    ozone : jnp.ndarray
+        Ozone concentration in lower stratosphere
+    stratz : jnp.ndarray
+        Polar night cooling in the stratosphere
+    zenit : jnp.ndarray
+        The Zenit angle
+    """
+
+    # Alpha = year phase (0 - 2pi, 0 = winter solstice = 22 Dec)
+    alpha = 4.0 * jnp.arcsin(1.0) * (tyear + 10.0 / 365.0)
+    dalpha = 0.0
+
+    coz1 = jnp.maximum(0.0, jnp.cos(alpha - dalpha))
+    coz2 = 1.8
+
+    azen = 1.0
+    nzen = 2
+
+    rzen = -jnp.cos(alpha) * 23.45 * jnp.arcsin(1.0) / 90.0
+
+    fs0 = 6.0
+
+    # Solar radiation at the top
+    topsr = jnp.zeros(il)
+    topsr = solar(tyear)
+    
+    def compute_fields(sia_j, coa_j, topsr_j):
+        flat2 = 1.5 * sia_j ** 2 - 0.5
+
+        # Solar radiation at the top
+        fsol_i_j = topsr_j
+
+        # Ozone depth in upper stratosphere
+        ozupp_i_j = 0.5 * epssw
+        ozone_i_j = 0.4 * epssw * (1.0 + coz1 * sia_j + coz2 * flat2)
+
+        # Zenith angle correction to (downward) absorptivity
+        zenit_i_j = 1.0 + azen * (1.0 - (coa_j * jnp.cos(rzen) + sia_j * jnp.sin(rzen))) ** nzen
+
+        # Ozone absorption in upper and lower stratosphere
+        ozupp_i_j = fsol_i_j * ozupp_i_j * zenit_i_j
+        ozone_i_j = fsol_i_j * ozone_i_j * zenit_i_j
+
+        # Polar night cooling in the stratosphere
+        stratz_i_j = jnp.maximum(fs0 - fsol_i_j, 0.0)
+
+        return jnp.full(ix, fsol_i_j), jnp.full(ix, ozupp_i_j), jnp.full(ix, ozone_i_j), jnp.full(ix, zenit_i_j), jnp.full(ix, stratz_i_j)
+
+    vectorized_compute_fields = vmap(compute_fields, in_axes=0, out_axes=1)
+
+    fsol, ozupp, ozone, zenit, stratz = vectorized_compute_fields(sia, coa, topsr)
+
+    return fsol, ozupp, ozone, zenit, stratz
+    
+
 def clouds(qa ,rh,precnv,precls,iptop,gse,fmask):
     #import params as p 
     from jcm.params import kx 
     '''
-    qa(ix,il,kx)   # Specific humidity [g/kg]
-    rh(ix,il,kx)   # Relative humidity
-    precnv(ix,il)  # Convection precipitation
-    precls(ix,il)  # Large-scale condensational precipitation
-    iptop(ix,il)
-    gse(ix,il)     # Vertical gradient of dry static energy
-    fmask(ix,il)   # Fraction land-sea mask
-    icltop(ix,il)  # Cloud top level
-    cloudc(ix,il)  # Total cloud cover
-    clstr(ix,il)   # Stratiform cloud cover
+    Simplified cloud cover scheme based on relative humidity and precipitation.
 
-    integer :: i, j, k, nl1, nlp
-    real(kind=8) :: clfact, clstrl, drh, fstab, pr1, rgse, rrcl
+    Args:
+        qa: Specific humidity [g/kg]
+        rh: Relative humidity
+        precnv: Convection precipitation
+        precls: Large-scale condensational precipitation
+        iptop: Cloud top level
+        gse: Vertical gradient of dry static energy
+        fmask: Fraction land-sea mask
+
+    Returns:
+        icltop: Cloud top level
+        cloudc: Total cloud cover
+        clstr: Stratiform cloud cover
+        
     '''
-    # these are all just for clouds - none are shared. clouds function doesn't need any shared variables (i dont think)
-    # these should get moved to shortwave_radiation_clouds and this file can be deleted
 
+    # Constants
     rhcl1   = 0.30  # Relative humidity threshold corresponding to cloud cover = 0
     rhcl2   = 1.00  # Relative humidity correponding to cloud cover = 1
     qacl    = 0.20  # Specific humidity threshold for cloud cover
@@ -39,10 +118,6 @@ def clouds(qa ,rh,precnv,precls,iptop,gse,fmask):
     clsminl = 0.15  # Minimum stratiform cloud cover over land (for RH = 1)
     gse_s0  = 0.25  # Gradient of dry static energy corresponding to stratiform cloud cover = 0
     gse_s1  = 0.40  # Gradient of dry static energy corresponding to stratiform cloud cover = 1
-
-    # icltop(p.ix,p.il)  # Cloud top level (make sure returns integer)
-    # cloudc(p.ix,p.il)  # Total cloud cover
-    # clstr(p.ix,p.il)   # Stratiform cloud cover
 
     nl1  = kx-2
     nlp  = kx
@@ -144,8 +219,6 @@ def solar(tyear):
     sh0 = jnp.sin(h0)
 
     topsr = csolp * fdis * (h0 * sia * sdecl + sh0 * coa * cdecl)
-    
 
     return topsr
-
 
