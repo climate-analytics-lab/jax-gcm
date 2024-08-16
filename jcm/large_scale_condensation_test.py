0777486aa446d3ec0cf37c0653a74e6395b2cf44
Duncan Watson-Parris <dwatsonparris@ucsd.edu>
2024-08-16 14:45:46 -0700

    Fixes to the large_scale condensation plus new test

--- jcm/large_scale_condensation_test.py
+++ jcm/large_scale_condensation_test.py
@@ -17,3 +17,21 @@ class TestLargeScaleCondensationUnit(unittest.TestCase):
         self.assertIsNotNone(precls)
         self.assertIsNotNone(dtlsc)
         self.assertIsNotNone(dqlsc)
+
+    def test_get_large_scale_condensation_tendencies_realistic(self):
+        ix, il, kx = 1, 1, 8
+        psa = jnp.ones((ix, il)) * 1.0110737
+        qa = jnp.asarray([[[16.148024  , 10.943978  ,  5.851813  ,  2.4522789 ,  0.02198645,
+        0.16069981,  0.        ,  0.        ]]])
+        qsat = jnp.asarray([[[1.64229703e-01, 1.69719307e-02, 1.45193088e-01, 1.98833509e+00,
+       4.58917155e+00, 9.24226425e+00, 1.48490220e+01, 2.02474803e+01]]])
+        itop = jnp.zeros((ix, il))
+
+        itop, precls, dtlsc, dqlsc = get_large_scale_condensation_tendencies(psa, qa, qsat, itop)
+        
+        self.assertAlmostEqual(precls, jnp.asarray([1.293]), delta=0.05)
+        self.assertAlmostEqual(dtlsc, jnp.asarray([0.00000000e+00, 1.59599063e-05, 7.07364228e-05, 1.45072684e-04,
+       0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00]))
+        self.assertAlmostEqual(dqlsc, jnp.asarray([ 0.00000000e+00, -7.59054545e-04, -3.98269278e-04, -5.82378946e-05,
+        0.00000000e+00,  0.00000000e+00,  0.00000000e+00,  0.00000000e+00]))
+
