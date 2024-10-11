65f8da70a0c97c89f012524779bbec72e6271fa4
eldavenport <ellendavenport7@gmail.com>
2024-10-11 10:29:12 -0700

    refactoring init functions to be more concise for physics_data"

--- jcm/physics_data.py
+++ jcm/physics_data.py
@@ -9,10 +9,7 @@ class DateData:
     tyear: jnp.ndarray # Fractional time of year, should possibly be part of the model itself (i.e. not in physics_data)
 
     def __init__(self, tyear=None) -> None:
-        if tyear is not None:
-            self.tyear = tyear
-        else:
-            self.tyear = jnp.zeros((1))
+        self.tyear = tyear if tyear is not None else jnp.zeros((1))
 
     def copy(self, tyear=None):
         return DateData(
@@ -25,22 +22,10 @@ class LWRadiationData:
     dfabs: jnp.ndarray # Flux of long-wave radiation absorbed in each atmospheric layer
     
     def __init__(self, nodal_shape, node_levels, rlds=None, dfabs=None, ftop=None, slr=None) -> None:
-        if rlds is not None:
-            self.rlds = rlds
-        else:
-            self.rlds = jnp.zeros((nodal_shape))
-        if dfabs is not None:
-            self.dfabs = dfabs
-        else:
-            self.dfabs = jnp.zeros((nodal_shape + (node_levels,)))
-        if ftop is not None:
-            self.ftop = ftop
-        else:
-            self.ftop = jnp.zeros((nodal_shape))
-        if slr is not None:
-            self.slr = slr
-        else:
-            self.slr = jnp.zeros((nodal_shape))
+        self.rlds = rlds if rlds is not None else jnp.zeros((nodal_shape))
+        self.dfabs = dfabs if dfabs is not None else jnp.zeros((nodal_shape + (node_levels,)))
+        self.ftop = ftop if ftop is not None else jnp.zeros((nodal_shape))
+        self.slr = slr if slr is not None else jnp.zeros((nodal_shape))
 
     def copy(self, rlds=None, dfabs=None, ftop=None, slr=None):
         return LWRadiationData(
@@ -70,63 +55,20 @@ class SWRadiationData:
     dfabs: jnp.ndarray #Flux of short-wave radiation absorbed in each atmospheric layer
 
     def __init__(self, nodal_shape, node_levels, qcloud=None, fsol=None, rsds=None, ssr=None, ozone=None, ozupp=None, zenit=None, stratz=None, gse=None, icltop=None, cloudc=None, cloudstr=None, ftop=None, dfabs=None) -> None:
-        if qcloud is not None:
-            self.qcloud = qcloud
-        else:
-            self.qcloud = jnp.zeros((nodal_shape))
-        if fsol is not None:
-            self.fsol = fsol
-        else:
-            self.fsol = jnp.zeros((nodal_shape))
-        if rsds is not None:
-            self.rsds = rsds
-        else:
-            self.rsds = jnp.zeros((nodal_shape))
-        if ssr is not None:
-            self.ssr = ssr
-        else:
-            self.ssr = jnp.zeros((nodal_shape))
-        if ozone is not None:
-            self.ozone = ozone
-        else:
-            self.ozone = jnp.zeros((nodal_shape))
-        if ozupp is not None:
-            self.ozupp = ozupp
-        else:
-            self.ozupp = jnp.zeros((nodal_shape))
-        if zenit is not None:
-            self.zenit = zenit
-        else:
-            self.zenit = jnp.zeros((nodal_shape))
-        if stratz is not None:
-            self.stratz = stratz
-        else:
-            self.stratz = jnp.zeros((nodal_shape))
-        if gse is not None:
-            self.gse = gse
-        else:
-            self.gse = jnp.zeros((nodal_shape))
-        if icltop is not None:
-            self.icltop = icltop
-        else:
-            self.icltop = jnp.zeros((nodal_shape))
-        if cloudc is not None:
-            self.cloudc = cloudc
-        else:
-            self.cloudc = jnp.zeros((nodal_shape))
-        if cloudstr is not None:
-            self.cloudstr = cloudstr
-        else:
-            self.cloudstr = jnp.zeros((nodal_shape))
-        if ftop is not None:
-            self.ftop = ftop
-        else:
-            self.ftop = jnp.zeros((nodal_shape))
-        if dfabs is not None:
-            self.dfabs = dfabs
-        else:
-            self.dfabs = jnp.zeros((nodal_shape + (node_levels,)))
-
+        self.qcloud = qcloud if qcloud is not None else jnp.zeros((nodal_shape))
+        self.fsol = fsol if fsol is not None else jnp.zeros((nodal_shape))
+        self.rsds = rsds if rsds is not None else jnp.zeros((nodal_shape))
+        self.ssr = ssr if ssr is not None else jnp.zeros((nodal_shape))
+        self.ozone = ozone if ozone is not None else jnp.zeros((nodal_shape))
+        self.ozupp = ozupp if ozupp is not None else jnp.zeros((nodal_shape))
+        self.zenit = zenit if zenit is not None else jnp.zeros((nodal_shape))
+        self.stratz = stratz if stratz is not None else jnp.zeros((nodal_shape))
+        self.gse = gse if gse is not None else jnp.zeros((nodal_shape))
+        self.icltop = icltop if icltop is not None else jnp.zeros((nodal_shape))
+        self.cloudc = cloudc if cloudc is not None else jnp.zeros((nodal_shape))
+        self.cloudstr = cloudstr if cloudstr is not None else jnp.zeros((nodal_shape))
+        self.ftop = ftop if ftop is not None else jnp.zeros((nodal_shape))
+        self.dfabs = dfabs if dfabs is not None else jnp.zeros((nodal_shape + (node_levels,)))
 
     def copy(self, qcloud=None, fsol=None, rsds=None, ssr=None, ozone=None, ozupp=None, zenit=None, stratz=None, gse=None, icltop=None, cloudc=None, cloudstr=None, ftop=None, dfabs=None):
         return SWRadiationData(
@@ -167,42 +109,15 @@ class ModRadConData:
 
 
     def __init__(self, nodal_shape, node_levels, fband=None,alb_l=None,alb_s=None,albsfc=None,snowc=None,tau2=None,st4a=None,stratc=None,flux=None) -> None:
-        if fband is not None:
-            self.fband = fband
-        else:
-            self.fband = jnp.zeros((n_temperatures,n_bands))
-        if alb_l is not None:
-            self.alb_l = alb_l
-        else:
-            self.alb_l = jnp.zeros((nodal_shape))
-        if alb_s is not None:
-            self.alb_s = alb_s
-        else:
-            self.alb_s = jnp.zeros((nodal_shape))
-        if albsfc is not None:
-            self.albsfc = albsfc
-        else:
-            self.albsfc = jnp.zeros((nodal_shape))
-        if snowc is not None:
-            self.snowc = snowc
-        else:
-            self.snowc = jnp.zeros((nodal_shape))
-        if tau2 is not None:
-            self.tau2 = tau2
-        else:
-            self.tau2 = jnp.zeros((nodal_shape+(node_levels,)+(4,)))
-        if st4a is not None:
-            self.st4a = st4a
-        else:
-            self.st4a = jnp.zeros((nodal_shape+(node_levels,)+(2,)))
-        if stratc is not None:
-            self.stratc = stratc
-        else:
-            self.stratc = jnp.zeros((nodal_shape+(2,)))
-        if flux is not None:
-            self.flux = flux
-        else:
-            self.flux = jnp.zeros((nodal_shape+(4,)))
+        self.fband = fband if fband is not None else jnp.zeros((n_temperatures,n_bands))
+        self.alb_l = alb_l if alb_l is not None else jnp.zeros((nodal_shape))
+        self.alb_s = alb_s if alb_s is not None else jnp.zeros((nodal_shape))
+        self.albsfc = albsfc if albsfc is not None else jnp.zeros((nodal_shape))
+        self.snowc = snowc if snowc is not None else jnp.zeros((nodal_shape))
+        self.tau2 = tau2 if tau2 is not None else jnp.zeros((nodal_shape+(node_levels,)+(4,)))
+        self.st4a = st4a if st4a is not None else jnp.zeros((nodal_shape+(node_levels,)+(2,)))
+        self.stratc = stratc if stratc is not None else jnp.zeros((nodal_shape+(2,)))
+        self.flux = flux if flux is not None else jnp.zeros((nodal_shape+(4,)))
 
     def copy(self,fband=None,alb_l=None,alb_s=None,albsfc=None,snowc=None,tau2=None,st4a=None,stratc=None,flux=None):
         return ModRadConData(
@@ -223,10 +138,7 @@ class SeaModelData:
     tsea: jnp.ndarray # SST, should come from sea_model.py
     
     def __init__(self, nodal_shape, tsea=None) -> None:
-        if tsea is not None:
-            self.tsea = tsea
-        else:
-            self.tsea = jnp.zeros((nodal_shape))
+        self.tsea = tsea if tsea is not None else jnp.zeros((nodal_shape))
 
     def copy(self, tsea=None):
         return CondensationData(
@@ -239,18 +151,9 @@ class CondensationData:
     precls: jnp.ndarray # Precipitation due to large-scale condensation
 
     def __init__(self, nodal_shape, node_levels, precls=None, dtlsc=None, dqlsc=None) -> None:
-        if precls is not None:
-            self.precls = precls
-        else:
-            self.precls = jnp.zeros((nodal_shape))
-        if dtlsc is not None:
-            self.dtlsc = dtlsc
-        else:
-            self.dtlsc = jnp.zeros((nodal_shape+(node_levels,)))
-        if dqlsc is not None:
-            self.dqlsc = dqlsc
-        else:
-            self.dqlsc = jnp.zeros((nodal_shape+(node_levels,)))
+        self.precls = precls if precls is not None else jnp.zeros((nodal_shape))
+        self.dtlsc = dtlsc if dtlsc is not None else jnp.zeros((nodal_shape+(node_levels,)))
+        self.dqlsc = dqlsc if dqlsc is not None else jnp.zeros((nodal_shape+(node_levels,)))
 
     def copy(self, precls=None, dtlsc=None, dqlsc=None):
         return CondensationData(
@@ -270,26 +173,11 @@ class ConvectionData:
     precnv: jnp.ndarray # Convective precipitation [g/(m^2 s)]
 
     def __init__(self, nodal_shape, node_levels, psa=None, se=None, iptop=None, cbmf=None, precnv=None) -> None:
-        if psa is not None:
-            self.psa = psa
-        else:
-            self.psa = jnp.zeros((nodal_shape))
-        if se is not None:
-            self.se = se
-        else:
-            self.se = jnp.zeros((nodal_shape + (node_levels,)))
-        if iptop is not None:
-            self.iptop = iptop
-        else:
-            self.iptop = jnp.zeros((nodal_shape),dtype=int)
-        if cbmf is not None:
-            self.cbmf = cbmf
-        else:
-            self.cbmf = jnp.zeros((nodal_shape))
-        if precnv is not None:
-            self.precnv = precnv
-        else:
-            self.precnv = jnp.zeros((nodal_shape))
+        self.psa = psa if psa is not None else jnp.zeros((nodal_shape))
+        self.se = se if se is not None else jnp.zeros((nodal_shape + (node_levels,)))
+        self.iptop = iptop if iptop is not None else jnp.zeros((nodal_shape),dtype=int)
+        self.cbmf = cbmf if cbmf is not None else jnp.zeros((nodal_shape))
+        self.precnv = precnv if precnv is not None else jnp.zeros((nodal_shape))
 
     def copy(self, psa=None, se=None, iptop=None, cbmf=None, precnv=None):
         return ConvectionData(
@@ -308,14 +196,8 @@ class HumidityData:
     qsat: jnp.ndarray # saturation specific humidity
 
     def __init__(self, nodal_shape, node_levels, rh=None, qsat=None) -> None:
-        if rh is not None:
-            self.rh = rh
-        else:
-            self.rh = jnp.zeros((nodal_shape+(node_levels,)))
-        if qsat is not None:
-            self.qsat = qsat
-        else:
-            self.qsat = jnp.zeros((nodal_shape+(node_levels,)))
+        self.rh = rh if rh is not None else jnp.zeros((nodal_shape+(node_levels,)))
+        self.qsat = qsat if qsat is not None else jnp.zeros((nodal_shape+(node_levels,)))
 
 
     def copy(self, rh=None, qsat=None):
@@ -347,70 +229,22 @@ class SurfaceFluxData:
     phi0: jnp.ndarray # Surface geopotential (i.e. orography), should come from boundaries.py
 
     def __init__(self, nodal_shape, stl_am=None, soilw_am=None, lfluxland=None, ustr=None, vstr=None, shf=None, evap=None, slru=None, hfluxn=None, tsfc=None, tskin=None, u0=None, v0=None, t0=None, fmask=None, phi0=None) -> None:
-        if stl_am is not None:
-            self.stl_am = stl_am
-        else:
-            self.stl_am = jnp.full((nodal_shape), 288.0)
-        if soilw_am is not None:
-            self.soilw_am = soilw_am
-        else:
-            self.soilw_am = jnp.full((nodal_shape), 0.5)
-        if lfluxland is not None:
-            self.lfluxland = lfluxland
-        else:
-            self.lfluxland = True
-        if ustr is not None:
-            self.ustr = ustr
-        else:
-            self.ustr = jnp.zeros((nodal_shape)+(3,))
-        if vstr is not None:
-            self.vstr = vstr
-        else:
-            self.vstr = jnp.zeros((nodal_shape)+(3,))
-        if shf is not None:
-            self.shf = shf
-        else:
-            self.shf = jnp.zeros((nodal_shape)+(3,))
-        if evap is not None:
-            self.evap = evap
-        else:
-            self.evap = jnp.zeros((nodal_shape)+(3,))
-        if slru is not None:
-            self.slru = slru
-        else:
-            self.slru = jnp.zeros((nodal_shape)+(3,))
-        if hfluxn is not None:
-            self.hfluxn = hfluxn
-        else:
-            self.hfluxn = jnp.zeros((nodal_shape)+(2,))
-        if tsfc is not None:
-            self.tsfc = tsfc
-        else:
-            self.tsfc = jnp.zeros((nodal_shape))
-        if tskin is not None:
-            self.tskin = tskin
-        else:
-            self.tskin = jnp.zeros((nodal_shape))
-        if u0 is not None:
-            self.u0 = u0
-        else:
-            self.u0 = jnp.zeros((nodal_shape))
-        if v0 is not None:
-            self.v0 = v0
-        else:
-            self.v0 = jnp.zeros((nodal_shape))
-        if t0 is not None:
-            self.t0 = t0
-        else:
-            self.t0 = jnp.zeros((nodal_shape))
-        if fmask is not None:
-            self.fmask = fmask
-        else:
-            self.fmask = jnp.zeros((nodal_shape))
-        if phi0 is not None:
-            self.phi0 = phi0
-        else:
-            self.phi0 = jnp.zeros((nodal_shape))
+        self.stl_am = stl_am if stl_am is not None else jnp.full((nodal_shape), 288.0)
+        self.soilw_am = soilw_am if soilw_am is not None else jnp.full((nodal_shape), 0.5)
+        self.lfluxland = lfluxland if lfluxland is not None else True
+        self.ustr = ustr if ustr is not None else jnp.zeros((nodal_shape)+(3,))
+        self.vstr = vstr if vstr is not None else jnp.zeros((nodal_shape)+(3,))
+        self.shf = shf if shf is not None else jnp.zeros((nodal_shape)+(3,))
+        self.evap = evap if evap is not None else jnp.zeros((nodal_shape)+(3,))
+        self.slru = slru if slru is not None else jnp.zeros((nodal_shape)+(3,))
+        self.hfluxn = hfluxn if hfluxn is not None else jnp.zeros((nodal_shape)+(2,))
+        self.tsfc = tsfc if tsfc is not None else jnp.zeros((nodal_shape))
+        self.tskin = tskin if tskin is not None else jnp.zeros((nodal_shape))
+        self.u0 = u0 if u0 is not None else jnp.zeros((nodal_shape))
+        self.v0 = v0 if v0 is not None else jnp.zeros((nodal_shape))
+        self.t0 = t0 if t0 is not None else jnp.zeros((nodal_shape))
+        self.fmask = fmask if fmask is not None else jnp.zeros((nodal_shape))
+        self.phi0 = phi0 if phi0 is not None else jnp.zeros((nodal_shape))
 
     
     def copy(self, stl_am=None, soilw_am=None, lfluxland=None, ustr=None, vstr=None, shf=None, evap=None, slru=None, hfluxn=None, tsfc=None, tskin=None, u0=None, v0=None, t0=None, fmask=None, phi0=None):
@@ -447,42 +281,15 @@ class PhysicsData:
     sea_model: SeaModelData
 
     def __init__(self, nodal_shape, node_levels,shortwave_rad=None, longwave_rad=None, convection=None, mod_radcon=None, humidity=None, condensation=None, surface_flux=None, date=None, sea_model=None) -> None:
-        if longwave_rad is not None:
-            self.longwave_rad = longwave_rad
-        else:
-            self.longwave_rad = LWRadiationData(nodal_shape, node_levels)
-        if shortwave_rad is not None:
-            self.shortwave_rad = shortwave_rad
-        else:
-            self.shortwave_rad = SWRadiationData(nodal_shape, node_levels)
-        if convection is not None:
-            self.convection = convection
-        else:
-            self.convection = ConvectionData(nodal_shape, node_levels)
-        if mod_radcon is not None:
-            self.mod_radcon = mod_radcon
-        else:
-            self.mod_radcon = ModRadConData(nodal_shape, node_levels)
-        if humidity is not None:
-            self.humidity = humidity
-        else:
-            self.humidity = HumidityData(nodal_shape, node_levels)
-        if condensation is not None:
-            self.condensation = condensation
-        else:
-            self.condensation = CondensationData(nodal_shape, node_levels)
-        if surface_flux is not None:
-            self.surface_flux = surface_flux
-        else:
-            self.surface_flux = SurfaceFluxData(nodal_shape)
-        if date is not None:
-            self.date = date
-        else:
-            self.date = DateData()
-        if sea_model is not None:
-            self.sea_model = sea_model
-        else:
-            self.sea_model = SeaModelData(nodal_shape)
+        self.longwave_rad = longwave_rad if longwave_rad is not None else LWRadiationData(nodal_shape, node_levels)
+        self.shortwave_rad = shortwave_rad if shortwave_rad is not None else SWRadiationData(nodal_shape, node_levels)
+        self.convection = convection if convection is not None else ConvectionData(nodal_shape, node_levels)
+        self.mod_radcon = mod_radcon if mod_radcon is not None else ModRadConData(nodal_shape, node_levels)
+        self.humidity = humidity if humidity is not None else HumidityData(nodal_shape, node_levels)
+        self.condensation = condensation if condensation is not None else CondensationData(nodal_shape, node_levels)
+        self.surface_flux = surface_flux if surface_flux is not None else SurfaceFluxData(nodal_shape)
+        self.date = date if date is not None else DateData()
+        self.sea_model = sea_model if sea_model is not None else SeaModelData(nodal_shape)
 
     def copy(self,shortwave_rad=None,longwave_rad=None,convection=None, mod_radcon=None, humidity=None, condensation=None, surface_flux=None, date=None, sea_model=None):
         return PhysicsData(
