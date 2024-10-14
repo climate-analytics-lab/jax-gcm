d5f458ab61c58a2a38416fdd7f4259bc28880c1c
J. Varan Madan <jamadan@ucsd.edu>
2024-10-13 18:29:55 -0700

    Fix refactor

--- jcm/model.py
+++ jcm/model.py
@@ -15,7 +15,7 @@ from jcm.humidity import spec_hum_to_rel_hum
 def convert_tendencies_to_equation(dynamics, physics_terms):
     def physical_tendencies(state):
         return get_physical_tendencies(state, dynamics, physics_terms)
-    return ExplicitODE.from_function(physical_tendencies)
+    return ExplicitODE.from_functions(physical_tendencies)
 
 
 class SpeedyModel:
@@ -48,11 +48,11 @@ class SpeedyModel:
             vertical=dinosaur.sigma_coordinates.SigmaCoordinates.equidistant(layers))
         
         # Not sure why we need this...
-        physics_specs = dinosaur.primitive_equations.PrimitiveEquationsSpecs.from_si()
+        self.physics_specs = dinosaur.primitive_equations.PrimitiveEquationsSpecs.from_si()
 
         self.inner_steps = int(save_every / dt_si)
         self.outer_steps = int(total_time / save_every)
-        self.dt = physics_specs.nondimensionalize(dt_si)
+        self.dt = self.physics_specs.nondimensionalize(dt_si)
 
         # Get the reference temerature and orography. This also returns the initial state function (if wanted to start from rest)
         p0 = 100e3 * units.pascal
@@ -60,20 +60,20 @@ class SpeedyModel:
 
         self.initial_state_fn, aux_features = dinosaur.primitive_equations_states.isothermal_rest_atmosphere(
             coords=self.coords,
-            physics_specs=physics_specs,
+            physics_specs=self.physics_specs,
             p0=p0,
             p1=p1)
         
-        ref_temps = aux_features[dinosaur.xarray_utils.REF_TEMP_KEY]
-        orography = dinosaur.primitive_equations.truncated_modal_orography(
+        self.ref_temps = aux_features[dinosaur.xarray_utils.REF_TEMP_KEY]
+        self.orography = dinosaur.primitive_equations.truncated_modal_orography(
             aux_features[dinosaur.xarray_utils.OROGRAPHY], self.coords)
 
         # Governing equations
         primitive = dinosaur.primitive_equations.PrimitiveEquations(
-            ref_temps,
-            orography,
+            self.ref_temps,
+            self.orography,
             self.coords,
-            physics_specs)
+            self.physics_specs)
 
         physics_terms = [
             spec_hum_to_rel_hum, # this could get called in get_physics_tendencies before looping over the physics terms
