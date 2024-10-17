38e3c1b218bd160b95c8bd191ae78ef92d4a49a4
J. Varan Madan <jamadan@ucsd.edu>
2024-10-17 13:44:44 -0700

    Change np to jnp

--- jcm/held_suarez_model.py
+++ jcm/held_suarez_model.py
@@ -78,7 +78,7 @@ class HeldSuarezModel:
         self.primitive_with_hs = dinosaur.time_integration.compose_equations([primitive, speedy_forcing])
 
         # Define trajectory times, expects start_with_input=False
-        self.times = save_every * np.arange(1, self.outer_steps+1)
+        self.times = save_every * jnp.arange(1, self.outer_steps+1)
 
         step_fn = dinosaur.time_integration.imex_rk_sil3(self.primitive_with_hs, self.dt)
         filters = [
