52f1f1c4a218382bb29cb6fa4d82a58c007ccbe4
J. Varan Madan <jamadan@ucsd.edu>
2024-10-24 20:22:44 -0700

    Changed geo.initialize_geo() to modify rather than return fields

--- jcm/geometry.py
+++ jcm/geometry.py
@@ -6,27 +6,26 @@ For storing all variables related to the model's grid space.
 import jax.numpy as jnp
 from jax import jit
 
-from jcm.params import kx, il, iy
 from jcm.physical_constants import akap, omega
 
 # Initializes all of the model geometry variables.
 @jit
-def initialize_geometry():
+def initialize_geometry(self, kx = 8, il = 64, iy = 32):
     # Definition of model levels
     if kx == 5:
-        hsg = jnp.array([0.000, 0.150, 0.350, 0.650, 0.900, 1.000])
+        self.hsg = jnp.array([0.000, 0.150, 0.350, 0.650, 0.900, 1.000])
     elif kx == 7:
-        hsg = jnp.array([0.020, 0.140, 0.260, 0.420, 0.600, 0.770, 0.900, 1.000])
+        self.hsg = jnp.array([0.020, 0.140, 0.260, 0.420, 0.600, 0.770, 0.900, 1.000])
     elif kx == 8:
-        hsg = jnp.array([0.000, 0.050, 0.140, 0.260, 0.420, 0.600, 0.770, 0.900, 1.000])
+        self.hsg = jnp.array([0.000, 0.050, 0.140, 0.260, 0.420, 0.600, 0.770, 0.900, 1.000])
 
     # Layer thicknesses and full (u,v,T) levels
-    dhs = hsg[1:] - hsg[:-1]
-    fsg = 0.5 * (hsg[1:] + hsg[:-1])
+    self.dhs = self.hsg[1:] - self.hsg[:-1]
+    self.fsg = 0.5 * (self.hsg[1:] + self.hsg[:-1])
 
     # Additional functions of sigma
-    dhsr = 0.5 / dhs
-    fsgr = akap / (2.0 * fsg)
+    self.dhsr = 0.5 / self.dhs
+    self.fsgr = akap / (2.0 * self.fsg)
 
     # Horizontal functions
 
@@ -34,20 +33,16 @@ def initialize_geometry():
     # NB: J=1 is Southernmost point!
     j = jnp.arange(1, iy + 1)
 
-    sia_half = jnp.cos(3.141592654 * (j - 0.25) / (il + 0.5))
-    coa_half = jnp.sqrt(1.0 - sia_half ** 2.0)
+    self.sia_half = jnp.cos(3.141592654 * (j - 0.25) / (il + 0.5))
+    self.coa_half = jnp.sqrt(1.0 - self.sia_half ** 2.0)
 
-    sia = jnp.concatenate((-sia_half, sia_half[::-1]), axis=0).ravel()
-    coa = jnp.concatenate((coa_half, coa_half[::-1]), axis=0).ravel()
-    radang = jnp.concatenate((-jnp.arcsin(sia_half), jnp.arcsin(sia_half)[::-1]), axis=0)
+    self.sia = jnp.concatenate((-self.sia_half, self.sia_half[::-1]), axis=0).ravel()
+    self.coa = jnp.concatenate((self.coa_half, self.coa_half[::-1]), axis=0).ravel()
+    self.radang = jnp.concatenate((-jnp.arcsin(self.sia_half), jnp.arcsin(self.sia_half)[::-1]), axis=0)
 
     # Expand cosine and its reciprocal to cover both hemispheres
-    cosg = jnp.repeat(coa_half, 2)
-    cosgr = 1. / cosg
-    cosgr2 = 1. / (cosg * cosg)
+    self.cosg = jnp.repeat(self.coa_half, 2)
+    self.cosgr = 1. / self.cosg
+    self.cosgr2 = 1. / (self.cosg * self.cosg)
 
-    coriol = 2.0 * omega * sia
-
-    return hsg, dhs, fsg, dhsr, fsgr, sia_half, coa_half, sia, coa, radang, cosg, cosgr, cosgr2, coriol
-
-hsg, dhs, fsg, dhsr, fsgr, sia_half, coa_half, sia, coa, radang, cosg, cosgr, cosgr2, coriol = initialize_geometry()
+    self.coriol = 2.0 * omega * self.sia
