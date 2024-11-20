48575c98662f73c4d75e6d890329a52557c8474e
Nho-way <97704525+NickHo2048@users.noreply.github.com>
2024-11-20 12:15:05 -0800

    Implemented Hydra Testing

--- jcm/main.py
+++ jcm/main.py
@@ -1,36 +1,39 @@
+import hydra
+from omegaconf import DictConfig
 from jcm.model import SpeedyModel
-import argparse
 from dinosaur import primitive_equations_states
 from dataclasses import asdict
 
-def parse_args():
-    # optional arguments
-    parser = argparse.ArgumentParser(description="Instantiate and run SpeedyModel.")
-    parser.add_argument('--time_step', type=int, default=10, help="Time step")
-    parser.add_argument('--save_interval', type=int, default=10, help="Save checkpoint after given interval")
-    parser.add_argument('--total_time', type=int, default=10, help="Total time")
-    parser.add_argument('--layers', type=int, default=8, help="Number of layers")
-   
-    return parser.parse_args()
+"""
+Now when running main, you can pass in arguments through the command line:
 
-if __name__ == "__main__":
-    args = parse_args()
+python main.py
+python main.py model.time_step=20
 
+By default, values are saved as:
+time_step: 10
+save_interval: 10
+total_time: 10
+layers: 8
+
+"""
+@hydra.main(version_base=None, config_path="config", config_name="config")
+def main(cfg: DictConfig):
     model = SpeedyModel(
-        time_step=args.time_step,
-        save_interval=args.save_interval,
-        total_time=args.total_time,
-        layers=args.layers
+        time_step=cfg.model.time_step,
+        save_interval=cfg.model.save_interval,
+        total_time=cfg.model.total_time,
+        layers=cfg.model.layers
     )
     
-    # Get the initial state
     state = model.get_initial_state()
     state.tracers = {
-            'specific_humidity': primitive_equations_states.gaussian_scalar(
-                model.coords, model.physics_specs)}
-    # Use the initial state to call unroll
+        'specific_humidity': primitive_equations_states.gaussian_scalar(
+            model.coords, model.physics_specs)}
+            
     final_state, predictions = model.unroll(state)
-    
     ds = model.data_to_xarray(asdict(predictions))
     ds.to_netcdf("model_state.nc")
-    
+
+if __name__ == "__main__":
+    main()
