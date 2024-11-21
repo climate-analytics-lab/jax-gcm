Fix phi scaling etc

jcm/model.py
@@ -29,7 +29,7 @@ def get_speedy_physics_terms(grid_shape, sea_coupling_flag=0):
     from jcm.vertical_diffusion import get_vertical_diffusion_tend
     physics_terms = [
         spec_hum_to_rel_hum,
-        get_convection_tendencies,
+        # get_convection_tendencies,
         get_large_scale_condensation_tendencies,
         clouds,
         get_zonal_average_fields,
@@ -125,7 +125,6 @@ class SpeedyModel:
         )
         
         ref_temps = aux_features[dinosaur.xarray_utils.REF_TEMP_KEY]
-        # ref_temps = jnp.linspace(270, ref_temps[-1], ref_temps.shape[0])
         orography = dinosaur.primitive_equations.truncated_modal_orography(
             aux_features[dinosaur.xarray_utils.OROGRAPHY], self.coords)
 

jcm/physics.py
@@ -114,13 +114,12 @@ def dynamics_state_to_physics_state(state: StateWithTime, dynamics: PrimitiveEqu
     log_sp = dynamics.coords.horizontal.to_nodal(state.log_surface_pressure)
     sp = jnp.exp(log_sp)
 
+    # note: surface pressure is nondimensionalized by assuming mean(log_sp) = 0, which means it uses a pressure scale of 1e5, different from the scales used by other quantities, so we don't dimensionalize it here
     u = dynamics.physics_specs.dimensionalize(u, units.meter / units.second).m
     v = dynamics.physics_specs.dimensionalize(v, units.meter / units.second).m
     t = dynamics.reference_temperature[:, jnp.newaxis, jnp.newaxis] + dynamics.physics_specs.dimensionalize(t, units.kelvin).m
     q = dynamics.physics_specs.dimensionalize(q, units.gram / units.kilogram).m
-    # FIXME: having issues with these
-    # phi = dynamics.physics_specs.dimensionalize(phi, units.meter ** 2 / units.second ** 2).m
-    # sp = dynamics.physics_specs.dimensionalize(sp, 1e5 * units.pascal).m
+    phi = dynamics.physics_specs.dimensionalize(phi, units.meter ** 2 / units.second ** 2).m
 
     physics_state = PhysicsState(
         u.transpose(1, 2, 0),