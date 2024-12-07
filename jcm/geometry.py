Scaling + stability + boundary condition fixes (#90)
* Add fixed ssts profile to physics data initialization

* Add jit, ssts, pressure fix, etc

* add notebook

* working on it

* Fix the issues

* Hopefully fix tests

* Update held suarez

* Fix physics test

* Remove unneeded refactor

* Syntax

* Working on it

* Disable print statements

* Revert breaking changes

* Fix

* Updates

* propagate timestep refactor

* Simplify q scaling

* Fix phi scaling etc

* Fix convection bug

* Update notebook

* Delete old notebooks

* Update outputs

* Cleanup

* Move notebook

* Revert date.py change

* Resolve PR comments

jcm/geometry.py
@@ -49,7 +49,7 @@ def initialize_geometry(kx = 8, il = 64):
     # NB: J=1 is Southernmost point!
     j = jnp.arange(1, iy + 1)
 
-    sia_half = jnp.cos(3.141592654 * (j - 0.25) / (il + 0.5))
+    sia_half = jnp.cos(jnp.pi * (j - 0.25) / (il + 0.5))
     coa_half = jnp.sqrt(1.0 - sia_half ** 2.0)
 
     sia = jnp.concatenate((-sia_half, sia_half[::-1]), axis=0).ravel()

jcm/longwave_radiation.py
@@ -1,3 +1,5 @@
+import jax
+from jax import jit
 import jax.numpy as jnp
 from jcm.physical_constants import sbc, wvi, grdscp
 from jcm.mod_radcon import epslw, emisfc, fband
@@ -6,6 +8,7 @@ from jcm.physics import PhysicsState, PhysicsTendency
 from jcm.physics_data import PhysicsData
 nband = 4
 
+@jit
 def get_downward_longwave_rad_fluxes(state: PhysicsState, physics_data: PhysicsData):
 
     """
@@ -99,6 +102,7 @@ def get_downward_longwave_rad_fluxes(state: PhysicsState, physics_data: PhysicsD
 
     return physics_tendencies, physics_data
 
+@jit
 def get_upward_longwave_rad_fluxes(state: PhysicsState, physics_data: PhysicsData):
     """
     Calculate the upward longwave radiation fluxes
@@ -164,7 +168,7 @@ def get_upward_longwave_rad_fluxes(state: PhysicsState, physics_data: PhysicsDat
     dfabs = dfabs.at[:,:,1].set(dfabs[:,:,1] - corlw2)
     ftop = corlw1 + corlw2
 
-    ftop = jnp.sum(flux, axis = -1)
+    ftop += jnp.sum(flux, axis = -1)
 
     longwave_out = physics_data.longwave_rad.copy(rlds=fsfc, ftop=ftop, dfabs=dfabs)
     mod_radcon_out = physics_data.mod_radcon.copy(st4a=st4a)