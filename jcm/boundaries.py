dd4576cdf5bc1223d36b99d391ba84189de455d5
Duncan Watson-Parris <dwatsonparris@ucsd.edu>
2025-01-14 17:36:14 -0800

    Adding BoundaryData object and initialization from input file

--- jcm/boundaries.py
+++ jcm/boundaries.py
@@ -1,39 +1,56 @@
 import jax.numpy as jnp
-from jax import jit
-from jcm.physical_constants import grav
-from jcm.params import mx, nx
+import tree_math
 
-#global variables 
-fmask = None #fractional land-sea mask (ix,il)
-phi0 = None  #surface geopotential (ix, il)
-phis0 = None #spectrally-filtered surface geopotential
-alb0 = None #bare-land annual mean albedo (ix,il)
+@tree_math.struct
+class Boundaries:
+    fmask: jnp.ndarray # fractional land-sea mask (ix,il)
+    phi0: jnp.ndarray  # surface geopotential (ix, il)
+    phis0: jnp.ndarray # spectrally-filtered surface geopotential
+    alb0: jnp.ndarray # bare-land annual mean albedo (ix,il)
+
 
 def initialize_boundaries(input_filename):
-    global phi0, phis0, fmask, alb0
+    """
+    Initialize the boundary conditions
+    """
+    from physical_constants import grav
+    import xarray as xr
 
     # Read surface geopotential (i.e. orography)
-    phi0 = grav*load_boundary_file(input_filename, "orog")
+    phi0 = grav* xr.open_dataset(input_filename)["orog"]
 
     # Also store spectrally truncated surface geopotential
     phis0 = spectral_truncation(phi0)
 
     # Read land-sea mask
-    fmask = load_boundary_file(input_filename, "lsm")
+    fmask = xr.open_dataset(input_filename, mask_and_scale=)["lsm"]
 
     # Annual-mean surface albedo
-    alb0 = load_boundary_file(input_filename, "alb")
+    alb0 = xr.open_dataset(input_filename)["alb"]
 
-
-
-def load_boundary_file(file_name, field_name):
-    # implement whatever python method we want for loading a variable into ix, il array from ncfile
-    field = None
-
-    # Fix undefined values
+    # The original code has a fortran function that does the following:
     # where (field <= -999) then field = 0.0 
+    # TODO: I don't think we need to do this because of the fcheck's that get done, but should double check
 
-    return field
+    return Boundaries(fmask=fmask, phi0=phi0, phis0=phis0, alb0=alb0)
+
+
+def spectral_truncation(fg1):
+    # what shouldl we be using for grid_to_spec/spec_to_grid? should that be from dinosaur?
+    fsp # (mx,nx)
+
+    fsp = grid_to_spec(fg1)
+
+    for n in range(nx):
+        for m in range(mx):
+            total_wavenumber = m + n - 2
+            if (total_wavenumber > trunc):
+                fsp[m,n] = (0.0, 0.0) #--> change to jax.at
+
+    fg2 = spec_to_grid(fsp, 1)
+
+    return fg2
+
 
 # land model and sea model call this 
 # Check consistency of surface fields with land-sea mask and set undefined 
@@ -46,21 +63,10 @@ def forchk(fmask, nf, fmin, fmax, fset, field):
         fset            # Replacement for undefined values
     '''
     
-    # need to adjust indices -- these are the fortran version 
-    ix, il = fmask.shape
-    for jf in range(nf):
-        nfault = 0
-
-        for i in range(ix):
-            for j in range(il):
-                if (fmask[i,j] > 0.0):
-                    if ((field[i,j,jf] < fmin) | (field[i,j,jf] > fmax)):
-                        nfault = nfault + 1
-                else:
-                    field[i,j,jf] = fset
+    field = jnp.where(field <= fmin, fset, field)
+    field = jnp.where(field >= fmax, fset, field)
 
     
-    return field
 
 # Replace missing values in surface fields.
 # It is assumed that non-missing values exist near the Equator.
