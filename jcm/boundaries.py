32b922f8db7c70744c56fcaa165f9c56ef2c3ea1
eldavenport <ellendavenport7@gmail.com>
2025-01-15 16:36:45 -0800

    updating boundaries data struct. adding a copy function which we need for now if we want to run a coupling step

--- jcm/boundaries.py
+++ jcm/boundaries.py
@@ -12,10 +12,31 @@ class BoundaryData:
 
     fmask_l: jnp.ndarray # land mask - set by land_model_init()
     stl_am: jnp.ndarray # land surface temperature (ix,il)
-    snowd_am: jnp.ndarray # snow depth (water equivalent) (ix,il)
-    soilw_am: jnp.ndarray # soil water availability (ix,il)
+    stl_lm: jnp.ndarray # land surface temperature (ix,il)
+    rhcapl: jnp.ndarray # 1/heat capacity (land)
+    cdland: jnp.ndarray # 1/dissipation time (land)
+    stlcl_ob: jnp.ndarray
+    snowcl_ob: jnp.ndarray # -- for all days - don't need _am, just grab from _ob
+    soilwcl_ob: jnp.ndarray
 
     fmask_s: jnp.ndarray # sea mask - set bt sea_model_init()
+
+    def copy(self,fmask=None,phi0=None,phis0=None,alb0=None,fmask_l=None,stl_am=None,stl_lm=None,rhcapl=None,cdland=None,stlcl_ob=None,snowcl_ob=None,soilwcl_ob=None,fmask_s=None):
+        return BoundaryData(
+            fmask=fmask if fmask is not None else self.fmask,
+            phi0=phi0 if phi0 is not None else self.phi0,
+            phis0=phis0 if phis0 is not None else self.phis0,
+            alb0=alb0 if alb0 is not None else self.alb0,
+            fmask_l=fmask_l if fmask_l is not None else self.fmask_l,
+            stl_am=stl_am if stl_am is not None else self.stl_am,
+            stl_lm=stl_lm if stl_lm is not None else self.stl_lm,
+            rhcapl=rhcapl if rhcapl is not None else self.rhcapl,
+            cdland=cdland if cdland is not None else self.cdland,
+            stlcl_ob=stlcl_ob if stlcl_ob is not None else self.stlcl_ob,
+            snowcl_ob=snowcl_ob if snowcl_ob is not None else self.snowcl_ob,
+            soilwcl_ob=soilwcl_ob if soilwcl_ob is not None else self.soilwcl_ob,
+            fmask_s=fmask_s if fmask_s is not None else self.fmask_s
+        )
 
 # this function should probably call the land_model_init and sea_model_init functions
 def initialize_boundaries(surface_filename, primitive, truncation_number):
--- jcm/land_model.py
+++ jcm/land_model.py
@@ -3,20 +3,27 @@ import jax.numpy as jnp
 # should these be moved to params.py?
 land_coupling_flag = 1 # Flag for land-coupling (0: off, 1: on)
 sd2sc = 60.0 # Snow depth (mm water) corresponding to snow cover = 1
+# Soil moisture parameters
+swcap = 0.30 # Soil wetness at field capacity (volume fraction)
+swwil = 0.17 # Soil wetness at wilting point  (volume fraction)
+thrsh = 0.1 # Threshold for land-sea mask definition (i.e. minimum fraction of either land or sea)
+# Model parameters (default values)
+depth_soil = 1.0 # Soil layer depth (m)
+depth_lice = 5.0 # Land-ice depth (m)
+tdland  = 40.0 # Dissipation time (days) for land-surface temp. anomalies
+flandmin = 1.0/3.0 # Minimum fraction of land for the definition of anomalies
+hcapl  = depth_soil*2.50e+6 # Heat capacities per m^2 (depth*heat_cap/m^3)
+hcapli = depth_lice*1.93e+6
 
-# need to pass this function the boundaries data object -- this should get called from either boundaries.py
-# or model.py
+# this should get called from either boundaries.py or model.py
 def land_model_init(land_filename, snow_filename, soil_filename, surface_filename, boundaries):
     import xarray as xr
-
-    # Soil moisture parameters
-    swcap = 0.30 # Soil wetness at field capacity (volume fraction)
-    swwil = 0.17 # Soil wetness at wilting point  (volume fraction)
-    thrsh = 0.1 # Threshold for land-sea mask definition (i.e. minimum fraction of either land or sea)
-
+    from params import delt
     # =========================================================================
     # Initialize land-surface boundary conditions
     # =========================================================================
+    # reading from the files - we want ix, il, nmonths. speedy uses a bunch of loops to read these in
+    # we might want to reformat speedy boundary files to make this smoother (and more flexible)
 
     # Fractional and binary land masks
     fmask_l = boundaries.fmask
@@ -27,15 +34,15 @@ def land_model_init(land_filename, snow_filename, soil_filename, surface_filenam
                         jnp.where(boundaries.fmask > (1.0 - thrsh), 1.0, fmask_l), 0.0)
 
     # Land-surface temperature
-    stl12 = jnp.asarray(xr.open_dataset(surface_filename)["stl"])
-    # simple sanity check
-    fillsf(stl12(:,:,month), 0.0)
-    forchk(bmask_l, 12, 0.0, 400.0, 273.0, stl12)
+    stlcl_ob = jnp.asarray(xr.open_dataset(surface_filename)["stl"])
+    # instead of using a check forchk, we check that 0.0 < stl12 < 400 and if it isn't we set it to 273
+    # this might not work exactly because stl12 is ix,il,12 and bmask is ix,il
+    stlcl_ob = jnp.where(bmask_l > 0.0 & (stlcl_ob < 0.0 | stlcl_ob > 400.0), 273.0, stlcl_ob)
 
     # Snow depth
-    snowd12 = jnp.asarray(xr.open_dataset(snow_filename)["snowd"])
-    # simple sanity check
-    forchk(bmask_l, 12, 0.0, 20000.0, 0.0, snowd12)
+    snowcl_ob = jnp.asarray(xr.open_dataset(snow_filename)["snowd"])
+    # simple sanity check - same method ras above for stl12 
+    snowcl_ob = jnp.where(bmask_l > 0.0 & (snowcl_ob < 0.0 | snowcl_ob > 20000.0), 0.0, snowcl_ob)
 
     # Read soil moisture and compute soil water availability using vegetation fraction
     # Read vegetation fraction
@@ -61,95 +68,54 @@ def land_model_init(land_filename, snow_filename, soil_filename, surface_filenam
     # Compute the intermediate max term
     max_term = jnp.maximum(0.0, swroot - swwil2)
     # Compute the soil water content
-    soilw12 = jnp.minimum(1.0, rsw * (swl1 + veg * max_term))
-
-    # Repeat the computed 2D array along a new axis for 12 months - dont need to do this we 
-    # will have all 12 months at once
-    # soilw12 = jnp.tile(soilw12[:, :, None], (1, 1, 12))
-
-    # sanity check
-    forchk(bmask_l, 12, 0.0, 10.0, 0.0, soilw12)
+    soilwcl_ob = jnp.minimum(1.0, rsw * (swl1 + veg * max_term))
+    # simple sanity check - same method ras above for stl12 
+    soilwcl_ob = jnp.where(bmask_l > 0.0 & (soilwcl_ob < 0.0 | soilwcl_ob > 10.0), 0.0, soilwcl_ob)
 
     # =========================================================================
     # Set heat capacities and dissipation times for soil and ice-sheet layers
     # =========================================================================
 
-    # Model parameters (default values)
-    depth_soil = 1.0 # Soil layer depth (m)
-    depth_lice = 5.0 # Land-ice depth (m)
-    tdland  = 40.0 # Dissipation time (days) for land-surface temp. anomalies
-    flandmin = 1.0/3.0 # Minimum fraction of land for the definition of anomalies
-    hcapl  = depth_soil*2.50e+6 # Heat capacities per m^2 (depth*heat_cap/m^3)
-    hcapli = depth_lice*1.93e+6
-
     # 2. Compute constant fields
     # Set domain mask (blank out sea points)
-    dmask(:,:) = 1.
+    dmask = jnp.ones_like(fmask_l)
+    dmask = jnp.where(fmask_l < flandmin, 0, dmask)
 
-    do j=1,il
-        do i=1,ix
-            if (fmask_l(i,j).lt.flandmin) dmask(i,j) = 0
+    # Set time_step/heat_capacity and dissipation fields
+    rhcapl = jnp.where(boundaries.alb0 < 0.4, delt / hcapl, delt / hcapli)
+    cdland = dmask*tdland/(1.0+dmask*tdland)
 
-    ! Set time_step/heat_capacity and dissipation fields
-    do j=1,il
-        do i=1,ix
-            if (alb0(i,j).lt.0.4) then
-                rhcapl(i,j) = delt/hcapl
-            else
-                rhcapl(i,j) = delt/hcapli
+    boundaries_new = boundaries.copy(rhcapl=rhcapl, cdland=cdland, fmask_l=fmask_l, stlcl_ob=stlcl_ob, snowcl_ob=snowcl_ob, soilwcl_ob=soilwcl_ob)
+    return boundaries_new
 
-    cdland(:,:) = dmask(:,:)*tdland/(1.+dmask(:,:)*tdland)
+# Exchanges fluxes between land and atmosphere.
+# day = index for day of the year -- get this from tyear
+def couple_land_atm(day, boundaries):
+    # Run the land model if the land model flags is switched on
+    if (land_coupling_flag == 1):
+        stl_lm = run_land_model(boundaries.stlcl_lm, boundaries.stlcl_ob[:,:,day], boundaries.cdland, boundaries.rhcapl)
+        stl_am = stl_lm
+    # Otherwise get the land surface from climatology
+    else:
+        stl_am = boundaries.stlcl_ob[:,:,day]
 
-#Exchanges fluxes between land and atmosphere.
-def couple_land_atm(day):
-    # day = the day (starting at 0 for the first time step)
-    # Fortran has these imports, we don't necessarily have them
-    from date import imont1 
-    from interpolation import forin5, forint
-
-    # Interpolate climatological fields to actual date
-    # Climatological land surface temperature
-    forin5(imont1, stl12, stlcl_ob)
-
-    # Climatological snow depth
-    forint(imont1, snowd12, snowdcl_ob)
-
-    # Climatological soil water availability
-    forint(imont1, soilw12, soilwcl_ob)
-
-    # If it's the first day then initialise the land surface
-    # temperature from climatology
-    if (day == 0) then
-        stl_lm = stlcl_ob
-        stl_am = stlcl_ob
-    else
-        # Run the land model if the land model flags is switched on
-        if (land_coupling_flag == 1) then
-            call run_land_model
-
-            stl_am = stl_lm
-        # Otherwise get the land surface from climatology
-        else
-            stl_am = stlcl_ob
-
-    # Always get snow depth and soil water availability from climatology
-    snowd_am = snowdcl_ob
-    soilw_am = soilwcl_ob
+    # need to set boundaries.snowd_am, soilw_am, stl_am
+    boundaries_new = boundaries.copy(stl_am=stl_am, stl_lm=stl_lm)
+    return boundaries_new
 
 #Integrates slab land-surface model for one day.
-def run_land_model():
+def run_land_model(stl_lm, stlcl_ob, cdland, rhcapl):
     from jcm.auxiliaries import hfluxn
-
-    # Surface temperature anomaly
-    real(p) :: tanom(ix,il)
 
     # Land-surface (soil/ice-sheet) layer
     # Anomaly w.r.t. final-time climatological temperature
     tanom = stl_lm - stlcl_ob
 
     # Time evolution of temperature anomaly
-    tanom = cdland*(tanom + rhcapl*hfluxn(:,:,1))
+    tanom = cdland*(tanom + rhcapl*hfluxn[:,:,0])
 
     # Full surface temperature at final time
     stl_lm = tanom + stlcl_ob
+
+    return stl_lm
 
