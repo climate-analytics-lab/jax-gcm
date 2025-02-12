90064db58ce4ce8acfe4c2ff74acd93d0d7d1a9c
eldavenport <ellendavenport7@gmail.com>
2025-02-12 11:26:07 -0800

    adding any_true check to Parameters

--- jcm/params.py
+++ jcm/params.py
@@ -281,3 +281,7 @@ class Parameters:
             land_model = self.land_model.isnan(),
         )
     
+    def any_true(self):
+        return tree_util.tree_reduce(lambda x, y: x or y, tree_util.tree_map(lambda x: jnp.any(x), self))
+
+    
