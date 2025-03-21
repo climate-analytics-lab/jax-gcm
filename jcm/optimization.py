450d96f83bcab3398a77e28b9d3d50a39089c1e4
rgjini <88095587+rgjini@users.noreply.github.com>
2025-03-21 16:32:40 -0700

    optimization setup completed and ready for testing

--- jcm/optimization.py
+++ jcm/optimization.py
@@ -15,4 +15,42 @@ def loss_function(theta, forward_model, y, R_inv_sqrt, args = ()):
         args: additional forward model function inputs
     '''
 
-    return 0.5*jnp.linalg.norm(R_inv_sqrt@(y - forward_model(theta, *args)))
+    return 0.5*jnp.linalg.norm(R_inv_sqrt*(y - forward_model(theta, *args)))**2
+
+def create_model(parameters, time_step = 30, save_interval = 1.0, total_time = 5.0, layers = 8): 
+    '''
+    Returns speedy model with given parameters and specifications
+
+    Args: 
+        parameters: parameters object type (jcm.paramters)
+        time_step: Model time step in minutes
+        save_interval: Save interval in days
+        total_time: Total integration time in days
+        layers: Number of vertical layers
+    '''
+    model = SpeedyModel(time_step = time_step, 
+                        save_interval = save_interval, 
+                        total_time = total_time, 
+                        layers = layers,
+                        parameters = parameters, 
+                        post_process = True)
+    return model
+
+def forward_model_wrapper(theta, theta_keys, state = None, parameters = None, args = ()):
+    '''
+    Returns forward model run collapsed into a single vector
+    '''
+    if parameters is None:
+        parameters = Parameters.default()
+
+    ii = 0
+    for attr, params in theta_keys.items():
+        for param in params:
+            setattr(getattr(parameters, attr), param, theta[ii])
+            ii += 1
+        
+    model = create_model(parameters, *args) 
+    if state is None: 
+        state = model.get_initial_state()
+    final_state, predictions = model.unroll(state)
+    return predictions['dynamics'].temperature_variation.flatten()  # fix shape of this
