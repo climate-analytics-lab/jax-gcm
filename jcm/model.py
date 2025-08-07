4e162a912d1941a7f52b2e29fb0d513387a6f946
Duncan Watson-Parris <dwatsonparris@ucsd.edu>
2025-08-07 16:54:54 -0700

    Adding SCM model class

--- jcm/model.py
+++ jcm/model.py
@@ -221,3 +221,170 @@ class Model:
         ).astype('datetime64[ns]')
         
         return pred_ds
+
+class SingleColumnModel:
+    """
+    A single column model that uses the Speedy physics on an aquaplanet.
+    This is a simplified version of the Model class, focusing on a single column of the atmosphere.
+    """
+
+    def __init__(self, time_step=30.0, save_interval=10.0, total_time=1200,
+                 start_date=None, layers=8, horizontal_resolution=31,
+                 coords: CoordinateSystem=None, boundaries: BoundaryData=None,
+                 initial_state: PhysicsState=None, physics: Physics=None) -> None:
+        """
+        Initialize the model with the given time step, save interval, and total time.
+        
+        Args:
+            time_step: Model time step in minutes
+            save_interval: Save interval in days
+            total_time: Total integration time in days
+            start_date: Start date of the simulation
+            layers: Number of vertical layers
+            horizontal_resolution: Horizontal resolution of the model (31, 42, 85, or 213)
+            coords: CoordinateSystem object describing model grid
+            boundaries: BoundaryData object describing surface boundary conditions
+            initial_state: Initial state of the model (PhysicsState object), optional
+            physics: Physics object describing the model physics
+        """
+        from datetime import datetime
+
+        # Integration settings
+        self.start_date = start_date or Timestamp.from_datetime(datetime(2000, 1, 1))
+        self.save_interval = save_interval * units.day
+        self.total_time = total_time * units.day
+        self.time_step = time_step
+        dt_si = self.time_step * units.minute
+
+        self.physics_specs = PHYSICS_SPECS
+
+        if coords is not None:
+            self.coords = coords
+            horizontal_resolution = coords.horizontal.total_wavenumbers - 2
+        else:
+            self.coords = get_coords(layers=layers, horizontal_resolution=horizontal_resolution)
+        self.geometry = Geometry.from_coords(self.coords)
+
+        self.inner_steps = int(self.save_interval.to(units.minute) / dt_si)
+        self.outer_steps = int(self.total_time / self.save_interval)
+        self.dt = self.physics_specs.nondimensionalize(dt_si)
+
+        if initial_state is not None:
+            self.initial_state = initial_state
+        else:
+            self.initial_state = None
+
+        
+        self.physics = physics or SpeedyPhysics()
+
+        # TODO: make the truncation number a parameter consistent with the grid shape
+        params_for_boundaries = (self.physics.parameters 
+                                 if (hasattr(self.physics, 'parameters') and isinstance(self.physics.parameters, Parameters))
+                                 else Parameters.default())
+        if boundaries is None:
+            truncated_orography = primitive_equations.truncated_modal_orography(aux_features[dinosaur.xarray_utils.OROGRAPHY], self.coords, wavenumbers_to_clip=2)
+            self.boundaries = default_boundaries(self.coords.horizontal, aux_features[dinosaur.xarray_utils.OROGRAPHY], params_for_boundaries)
+        else:
+            self.boundaries = update_boundaries_with_timestep(boundaries, params_for_boundaries, dt_si)
+            truncated_orography = primitive_equations.truncated_modal_orography(self.boundaries.orog, self.coords, wavenumbers_to_clip=2)        
+        
+        physics_forcing_eqn = ExplicitODE.from_functions(lambda state:
+            get_physical_tendencies(
+                state=state,
+                dynamics=self.primitive,
+                time_step=time_step,
+                physics=self.physics,
+                boundaries=self.boundaries,
+                geometry=self.geometry,
+                date = DateData.set_date(
+                    model_time = self.start_date + Timedelta(seconds=state.sim_time),
+                    model_step = ((state.sim_time/60) / time_step).astype(jnp.int32)
+                )
+            )
+        )
+
+        # Define trajectory times, expects start_with_input=False
+        self.times = self.save_interval * jnp.arange(1, self.outer_steps+1)
+        
+        self.primitive_with_speedy = dinosaur.time_integration.compose_equations([self.primitive, physics_forcing_eqn])
+        step_fn = dinosaur.time_integration.imex_rk_sil3(self.primitive_with_speedy, self.dt)
+        filters = [
+            dinosaur.time_integration.exponential_step_filter(
+                self.coords.horizontal, self.dt, tau=0.0087504, order=1.5, cutoff=0.8
+            ),
+        ]
+        self.step_fn = dinosaur.time_integration.step_with_filters(step_fn, filters)
+
+    def get_initial_state(self, random_seed=0, sim_time=0.0, humidity_perturbation=False) -> primitive_equations.State:
+        from jcm.physics_interface import physics_state_to_dynamics_state
+
+        # Either use the designated initial state, or generate one. The initial state to the dycore is a modal primitive_equations.State,
+        # but the optional initial state from the user is a nodal PhysicsState
+        if self.initial_state is not None:
+            state = physics_state_to_dynamics_state(self.initial_state, self.primitive)
+        else:
+            state = self.default_state_fn(jax.random.PRNGKey(random_seed))
+            # default state returns log surface pressure, we want it to be log(normalized_surface_pressure)
+            # there are several ways to do this operation (in modal vs nodal space, with log vs absolute pressure), this one has the least error
+            state.log_surface_pressure = self.coords.horizontal.to_modal(
+                self.coords.horizontal.to_nodal(state.log_surface_pressure) - jnp.log(self.physics_specs.nondimensionalize(p0 * units.pascal)) # Makes this robust to different physics_specs, which will change default_state_fn behavior
+            )
+
+            # need to add specific humidity as a tracer
+            state.tracers = {
+                'specific_humidity': (1e-2 if humidity_perturbation else 0.0) * primitive_equations_states.gaussian_scalar(self.coords, self.physics_specs)
+            }
+        return primitive_equations.State(**state.asdict(), sim_time=sim_time)
+
+    def post_process(self, state: primitive_equations.State) -> Predictions:
+        from jcm.date import DateData
+        from jcm.physics_interface import dynamics_state_to_physics_state, verify_state
+
+        physics_state = dynamics_state_to_physics_state(state, self.primitive)
+        
+        physics_data = None
+        if self.physics.write_output:
+            date=DateData.set_date(
+                model_time = self.start_date + Timedelta(seconds=state.sim_time),
+                model_step = ((state.sim_time/60) / self.time_step).astype(jnp.int32)
+            )
+            clamped_physics_state = verify_state(physics_state)
+            _, physics_data = self.physics.compute_tendencies(clamped_physics_state, self.boundaries, self.geometry, date)
+
+        return Predictions(dynamics=physics_state, physics=physics_data)
+
+    def unroll(self, state: primitive_equations.State) -> tuple[primitive_equations.State, Predictions]:
+        integrate_fn = jax.jit(dinosaur.time_integration.trajectory_from_step(
+            jax.checkpoint(self.step_fn),
+            outer_steps=self.outer_steps,
+            inner_steps=self.inner_steps,
+            start_with_input=True,
+            post_process_fn=self.post_process,
+        ))
+        return integrate_fn(state)
+
+    def data_to_xarray(self, data):
+        from dinosaur.xarray_utils import data_to_xarray
+        return data_to_xarray(data, coords=self.coords, times=self.times)
+
+    def predictions_to_xarray(self, predictions):
+        # extract dynamics predictions (PhysicsState format)
+        # and physics predictions (PhysicsData format) from postprocessed output
+        dynamics_predictions = predictions.dynamics
+        physics_predictions = predictions.physics
+
+        # prepare physics predictions for xarray conversion
+        # (e.g. separate multi-channel fields so they are compatible with data_to_xarray)
+        physics_preds_dict = self.physics.data_struct_to_dict(physics_predictions, self.geometry)
+        
+        pred_ds = self.data_to_xarray(dynamics_predictions.asdict() | physics_preds_dict)
+        
+        # Flip the vertical dimension so that it goes from the surface to the top of the atmosphere
+        pred_ds = pred_ds.isel(level=slice(None, None, -1))
+
+        # convert time in days to datetime
+        pred_ds['time'] = (
+            (self.start_date.delta.days + pred_ds.time - self.save_interval.m)*(timedelta64(1, 'D')/timedelta64(1, 'ns'))
+        ).astype('datetime64[ns]')
+        
+        return pred_ds
