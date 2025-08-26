b19a193f944dc62e4909d13919cf4ce18851b95e
eldavenport <ellendavenport7@gmail.com>
2025-08-26 16:32:28 -0700

    using jax types for filter params breaks the filter

--- jcm/diffusion.py
+++ jcm/diffusion.py
@@ -4,18 +4,18 @@ from jax import tree_util
 
 @tree_math.struct
 class DiffusionFilter:
-    tendency_diff_timescale: jnp.ndarray # Diffusion timescale (s)
-    tendency_diff_order: jnp.ndarray # Order of diffusion operator for tendencies
-    state_diff_timescale: jnp.ndarray # Diffusion timescale (s)
-    state_diff_order: jnp.ndarray  # Order of diffusion operator for state variables
+    tendency_diff_timescale: float # Diffusion timescale (s)
+    tendency_diff_order: int # Order of diffusion operator for tendencies
+    state_diff_timescale: float # Diffusion timescale (s)
+    state_diff_order: int  # Order of diffusion operator for state variables
 
     @classmethod
     def default(cls):
         return cls(
-            tendency_diff_timescale = jnp.array(2.4*60*60), # Diffusion timescale (s)
-            tendency_diff_order = jnp.array(4), # Order of diffusion operator for tendencies
-            state_diff_timescale = jnp.array(2.4*60*60), # Diffusion timescale (s)
-            state_diff_order = jnp.array(1),  # Order of diffusion operator for state variables
+            tendency_diff_timescale = 2.4*60*60, # Diffusion timescale (s)
+            tendency_diff_order = 4, # Order of diffusion operator for tendencies
+            state_diff_timescale = 2.4*60*60, # Diffusion timescale (s)
+            state_diff_order = 1,  # Order of diffusion operator for state variables
         )
 
     def isnan(self):
