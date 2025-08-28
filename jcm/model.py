Fix blowup issue by normalizing during accumulation + add test

jcm/model.py
@@ -39,16 +39,18 @@ class DiagnosticsCollector(nnx.Module):
     data: nnx.Variable
     i: nnx.Variable
     physical_step: nnx.Variable
+    steps_to_average: int
 
-    def __init__(self):
+    def __init__(self, steps_to_average):
         self.data = None
         self.i = nnx.Variable(0)
         self.physical_step = nnx.Variable(True)
-    
+        self.steps_to_average = steps_to_average
+
     def accumulate_if_physical_step(self, new_data):
         if self.physical_step.value:
             self.data.value = tree_map(
-                lambda stacked_array, new_array: stacked_array.at[self.i.value].add(new_array.astype(jnp.float32)),
+                lambda stacked_array, new_array: stacked_array.at[self.i.value].add(new_array/self.steps_to_average),
                 self.data.value,
                 new_data
             )
@@ -79,7 +81,7 @@ def averaged_trajectory_from_step(
         (2) trajectory of length `outer_steps` representing time evolution (averaged over the inner steps between each outer step).
     """
     def integrate(x_initial, empty_data):
-        diagnostics_collector = DiagnosticsCollector()
+        diagnostics_collector = DiagnosticsCollector(steps_to_average=inner_steps)
         diagnostics_collector.data = nnx.Variable(stack_trees([empty_data] * outer_steps))
         graphdef, init_diag_state = nnx.split(diagnostics_collector)
 
@@ -107,7 +109,7 @@ def averaged_trajectory_from_step(
         (x_final, _, final_diag_state), (preds,) = outer_step(carry)
         return x_final, Predictions(
             dynamics=preds,
-            physics=tree_map(lambda x: (x / inner_steps), nnx.merge(graphdef, final_diag_state).data.value)
+            physics=nnx.merge(graphdef, final_diag_state).data.value
         )
 
     return integrate

jcm/model_test.py
@@ -1,6 +1,7 @@
 import unittest
 import jax.tree_util as jtu
 from dinosaur import primitive_equations_states
+import numpy as np
 
 class TestModelUnit(unittest.TestCase):
     def setUp(self):
@@ -57,9 +58,9 @@ class TestModelUnit(unittest.TestCase):
 
         # optionally add a boundary conditions file
         model = Model(
-            time_step=720,
-            save_interval=1,
-            total_time=2,
+            time_step=30,
+            save_interval=1/24.,
+            total_time=2/24.,
         )
     
         state = model.get_initial_state()
@@ -101,7 +102,32 @@ class TestModelUnit(unittest.TestCase):
         self.assertTupleEqual(dynamics_predictions.specific_humidity.shape, nodal_tzxy)
         self.assertTupleEqual(dynamics_predictions.geopotential.shape, nodal_tzxy)
         self.assertTupleEqual(dynamics_predictions.normalized_surface_pressure.shape, (nodal_tzxy[0],) + nodal_tzxy[2:])
-        
+
+    def test_speedy_model_averages(self):
+        from jcm.model import Model
+
+        model = Model(
+            time_step=30, # make sure this test stays valid if we ever change the default timestep
+            save_interval=.5/24.,
+            total_time=2/24.,
+        )
+        _, preds = model.unroll(model.get_initial_state())
+        true_avg_preds = jtu.tree_map(lambda a: np.mean(a, axis=0), preds)
+
+        avg_model = Model(
+            time_step=30,
+            save_interval=2/24.,
+            total_time=2/24.,
+            output_averages=True,
+        )
+        _, avg_preds = avg_model.unroll(avg_model.get_initial_state())
+
+        jtu.tree_map(
+            lambda a1, a2: self.assertTrue(np.allclose(a1, a2, atol=1e-4)),
+            true_avg_preds,
+            avg_preds
+        )
+
     def test_speedy_model_gradients_isnan(self):
         import jax
         import jax.numpy as jnp