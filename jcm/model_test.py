278b268570e9081fa2758afbb1cf1b42647d3c3e
Ellen Davenport <62270081+eldavenport@users.noreply.github.com>
2025-10-09 14:28:36 -0700

    create slow marker for pytests (#257)
    * adding pytest.ini for slow tests and marking gradient tests as slow
    
    * updating github workflow
    
    * updating the github workflow to cancel in progress tests if they are out of date
    
    * updating workflow not to run push and PR tests on open PRs
    
    * removing flag for open PR since it doesn't work
    
    * fixing workflow so that fast tests run on pushes, slow tests run on PRs (and so both run on open PRs)
    
    * testing github workflow
    
    * clarifying what a slow test is
    
    * figuring out the interaction between cancelling runs and running all tests
    
    * fixing formatting

--- jcm/model_test.py
+++ jcm/model_test.py
@@ -1,5 +1,6 @@
 import unittest
 import jax.tree_util as jtu
+import pytest
 
 class TestModelUnit(unittest.TestCase):
     def setUp(self):
@@ -96,7 +97,8 @@ class TestModelUnit(unittest.TestCase):
         self.assertTupleEqual(dynamics_predictions.specific_humidity.shape, nodal_tzxy)
         self.assertTupleEqual(dynamics_predictions.geopotential.shape, nodal_tzxy)
         self.assertTupleEqual(dynamics_predictions.normalized_surface_pressure.shape, (nodal_tzxy[0],) + nodal_tzxy[2:])
-        
+    
+    @pytest.mark.slow
     def test_speedy_model_gradients_isnan(self):
         import jax
         import jax.numpy as jnp
@@ -126,6 +128,7 @@ class TestModelUnit(unittest.TestCase):
         self.assertFalse(jnp.any(jnp.isnan(df_dstate[0].tracers['specific_humidity'])))
         # self.assertFalse(jnp.any(jnp.isnan(df_dstate[0].sim_time))) FIXME: this is ending up nan
 
+    @pytest.mark.slow
     def test_speedy_model_gradients_multiple_timesteps_isnan(self):
         import jax
         import jax.numpy as jnp
@@ -151,6 +154,7 @@ class TestModelUnit(unittest.TestCase):
         self.assertFalse(jnp.any(jnp.isnan(df_dstate[0].tracers['specific_humidity'])))
         # self.assertFalse(jnp.any(jnp.isnan(df_dstate[0].sim_time))) FIXME: this is ending up nan
 
+    @pytest.mark.slow
     def test_speedy_model_param_gradients_isnan_vjp(self):
         import jax
         from jcm.model import Model, get_coords
@@ -184,6 +188,7 @@ class TestModelUnit(unittest.TestCase):
 
         self.assertFalse(df_dparams[0].isnan().any_true())
     
+    @pytest.mark.slow
     def test_speedy_model_param_gradients_isnan_jvp(self):
         import jax
         import jax.numpy as jnp
