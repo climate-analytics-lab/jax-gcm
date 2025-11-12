699b898e1d94a3f4be938569f4e001081e65f2de
J. Varan Madan <jamadan@ucsd.edu>
2025-11-12 11:29:22 -0500

    Clean up ones_like_tangent logic

--- jcm/utils.py
+++ jcm/utils.py
@@ -1,4 +1,6 @@
+import jax
 import jax.numpy as jnp
+import numpy as np
 from jax import jit
 from jax.tree_util import tree_map
 from dinosaur.coordinate_systems import HorizontalGridTypes
@@ -33,22 +35,32 @@ def ones_like(x):
 def stack_trees(trees):
     return tree_map(lambda *arrays: jnp.stack(arrays, axis=0).astype(jnp.float32), *trees)
 
-def index_if_3d(arr, key):
+def _index_if_3d(arr, key):
     return arr[:, :, key] if arr.ndim > 2 else arr
 
 def tree_index_3d(tree, key):
-    return tree_map(lambda arr: index_if_3d(arr, key), tree)
+    return tree_map(lambda arr: _index_if_3d(arr, key), tree)
 
-# Convert object to float 
-def check_type_convert_to_float(x):
+def _check_type_ones_like_tangent(x):
+        if jnp.result_type(x) == jnp.float32:
+            return jnp.ones_like(x)
+        # in case of a bool or int, return a float0 denoting the lack of tangent space
+        # jax requires that we use numpy to construct the float0 scalar
+        # because it is a semantic placeholder not backed by any array data / memory allocation
+        return np.ones((), dtype=jax.dtypes.float0)
+
+def ones_like_tangent(pytree):
+    return tree_map(_check_type_ones_like_tangent, pytree)
+
+def _check_type_convert_to_float(x):
     return jnp.asarray(x, dtype=jnp.float32)
 
 def convert_to_float(x): 
-    return tree_map(check_type_convert_to_float, x)
+    return tree_map(_check_type_convert_to_float, x)
 
 # Revert object with type float back to true type
-def check_type_convert_back(x, x0):
+def _check_type_convert_back(x, x0):
     return x if jnp.result_type(x0) == jnp.float32 else x0
 
 def convert_back(x, x0):
-    return tree_map(check_type_convert_back, x, x0)
+    return tree_map(_check_type_convert_back, x, x0)
