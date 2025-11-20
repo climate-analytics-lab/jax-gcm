34fc61ffcaf76fd658230f7ec1eaed2d0d698dfe
Duncan Watson-Parris <dwatsonparris@ucsd.edu>
2025-11-20 13:51:18 -0800

    Don't serialize coord attrs

--- jcm/model.py
+++ jcm/model.py
@@ -457,7 +457,9 @@ class Model:
         times = jax.device_get(predictions.times)
         coords = jax.device_get(self.coords)
 
-        pred_ds = data_to_xarray(dynamics_predictions.asdict() | physics_preds_dict, coords=coords, times=times - times[0])
+        pred_ds = data_to_xarray(dynamics_predictions.asdict() | physics_preds_dict, 
+                                 coords=coords, serialize_coords_to_attrs=False,
+                                 times=times - times[0])
 
         # Import units attribute associated with each xarray output from units_table.csv
         units_df = pd.read_csv(Path(__file__).parent.parent / "jcm" / "physics" / "speedy" / "units_table.csv")
